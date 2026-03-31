638633f978906a110a9469c501e0f2063a7b140f
Eduarda Trindade :) <eduarda.trindade@escolar.ifrn.edu.br>
2026-03-31 16:10:56 -0300

    Aula 04

---
+++ Tiago - Projeto1/Assets/SourceFiles/Scripts/AudioPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    public List<AudioClip> audioClips = new List<AudioClip>();
+    public int selectedIndex = 0;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+            audioSource = gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    public void PlaySelected()
+    {
+        if (selectedIndex >= 0 && selectedIndex < audioClips.Count)
+        {
+            AudioManager.instance.Play(audioClips[selectedIndex], audioSource);
+        }
+    }
+
+    public void Stop()
+    {
+        AudioManager.instance.Stop(audioSource);
+    }
+
+    public void Pause()
+    {
+        AudioManager.instance.Pause();
+    }
+
+    public void Resume()
+    {
+        AudioManager.instance.Resume();
+    }
+}
